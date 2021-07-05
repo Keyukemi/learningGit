0587f669f36d855b83ba078a9644fa14c722ff22
Keyukemi <keyukemi.ku@gmail.com>
2021-07-05 12:09:01 +0100

    committing files

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+
+// mainImg = document.getElementById('mainImg');
+
+// thumb2 = document.getElementById('thumb2');
+// thumb2Src = document.getElementById('thumb2').src;
+
+// thumb2.addEventListener("click",function(){
+//     mainImg.src = thumb2Src
+// })
+
+// const current = document.querySelector('#current');
+// const imgs = document.querySelector('.imgs');
+// const img = document.querySelectorAll('.imgs img');
+// const opacity = 0.6;
+
+// // Set first img opacity
+// img[0].style.opacity = opacity;
+
+// imgs.addEventListener('click', imgClick);
+
+// function imgClick(e) {
+//   // Reset the opacity
+//   img.forEach(img => (img.style.opacity = 1));
+
+//   // Change current image to src of clicked image
+//   current.src = e.target.src;
+
+//   // Add fade in class
+//   current.classList.add('fade-in');
+
+//   // Remove fade-in class after .5 seconds
+//   setTimeout(() => current.classList.remove('fade-in'), 500);
+
+//   // Change the opacity to opacity var
+//   e.target.style.opacity = opacity;
+// }
